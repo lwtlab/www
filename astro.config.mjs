8ac4fcdb6f0304f330c464f253a67225f10e887b
ChenFeng <phonechan@foxmail.com>
2024-03-21 10:15:01 +0800

    init project

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://lwtlab.github.io",
+  base: "www",
+  integrations: [
+    starlight({
+      title: "Thucydides",
+      social: {
+        youtube: "https://youtube.com/thucydides",
+        "x.com": "https://x.com/thucydides",
+        github: "https://github.com/lwtlab",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
