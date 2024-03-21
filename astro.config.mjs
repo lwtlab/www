d3bd960bdbe2783042627569a9ac71d1fe53c32d
ChenFeng <phonechan@foxmail.com>
2024-03-21 10:48:17 +0800

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.thucydides.net",
-  base: "www",
   integrations: [
     starlight({
       title: "Thucydides",
