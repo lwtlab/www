94d8dd7a9c39c3861c92e8fb1dfa3289b092752b
ChenFeng <phonechan@foxmail.com>
2024-03-21 10:41:03 +0800

    CNAME 配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://lwtlab.github.io",
+  site: "https://www.thucydides.net",
   base: "www",
   integrations: [
     starlight({
